7dedf256334aa7689f2b3a0481df8474335ee6b1
Gilberto Echeverria <gilberto@esporaestudio.com>
2016-11-22 14:13:56 -0600

    Changed the name to char*

--- Hero.cpp
+++ Hero.cpp
@@ -1,13 +1,14 @@
 #include "Hero.hpp"
 
-Hero::Hero(std::string _name, int _HP, int _MP, int _speed, int _character_class,
+Hero::Hero(char * _name, int _HP, int _MP, int _speed, int _character_class,
+//Hero::Hero(std::string _name, int _HP, int _MP, int _speed, int _character_class,
         int _attack, int _defense, int _magic, double _attack_percent,
         double _defense_percent, double _magic_percent, double _critical_percent) :
     // Call the constructor for the character class with the required parameters
     Character(_HP, _MP, _speed, _character_class, _attack, _defense, _magic,
             _attack_percent, _defense_percent, _magic_percent, _critical_percent)
 {
-    name = _name;
+    //name = _name;
     experience = 0;
     gold = 0;
 }
@@ -22,7 +23,17 @@ void Hero::print()
 void Hero::print(int line)
 {
     if (line == 0)
-        std::cout << "== " << std::setw(9) << name << " ==";
+        std::cout << "== " << std::setw(NAME_LENGTH) << name << " ==";
     else
         Character::print(line);
+}
+
+void Hero::inputData()
+{
+    std::cout << "== Hero creation module ==\n";
+
+    std::cout << "Enter the name: ";
+    std::cin >> name;
+
+    Character::inputData();
 }
--- Hero.hpp
+++ Hero.hpp
@@ -11,18 +11,22 @@
 
 #include "Character.hpp"
 
+#define NAME_LENGTH 9
+
 class Hero : public Character {
     private:
-        std::string name;
         int experience;
         int gold;
         //int AP;    // Ability points
         //- inventory
+        char name[NAME_LENGTH];
+        //std::string name;
 
     public:
         // Constructor
         Hero() {}
-        Hero(std::string _name, int _HP, int _MP, int _speed, int _character_class,
+        Hero(char * _name, int _HP, int _MP, int _speed, int _character_class,
+        //Hero(std::string _name, int _HP, int _MP, int _speed, int _character_class,
                 int _attack, int _defense, int _magic, double _attack_percent,
                 double _defense_percent, double _magic_percent, double _critical_percent);
         /*
@@ -32,8 +36,10 @@ class Hero : public Character {
         void levelUP();
         */
         // Access methods
-        std::string getName() { return name; }
+        char * getName() { return name; }
+        //std::string getName() { return name; }
         // Helper methods
         void print();
         void print(int line);
+        void inputData();
 };
